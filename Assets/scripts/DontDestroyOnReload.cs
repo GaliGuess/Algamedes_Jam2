c670ac0663ea573d3f9d09a47dafe7e164b2e8c1
GaliGuess <zgal10@gmail.com>
2018-01-10 16:56:28 +0200

    music doesnt restart on scene reload

---
+++ Assets/scripts/DontDestroyOnReload.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DontDestroyOnReload : MonoBehaviour
+{
+	private static DontDestroyOnReload instance;
+	
+	void Awake() {
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		
+	}
+}
