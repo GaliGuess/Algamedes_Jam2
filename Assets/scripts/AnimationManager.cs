09f362615d1f8e84329c5507e05b58305d75069d
mosem <mandelmoshe@gmail.com>
2018-02-06 00:13:24 +0200

    start screen animation done

---
+++ Assets/scripts/AnimationManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour {
+
+	GameObject chars;
+
+	void Awake() {
+		chars = transform.parent.Find("chars").gameObject;
+	}
+
+	// Use this for initialization
+	void Start () {
+		chars.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDone() {
+		chars.SetActive(true);
+		gameObject.SetActive(false);
+	}
+}
