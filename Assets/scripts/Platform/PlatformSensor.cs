6312c46dd05acb3c9e9d51004f7ce9357443398d
mosem <mandelmoshe@gmail.com>
2018-02-06 17:07:11 +0200

    added shells to platform carrier - allow shells to be moved by moving platforms

--- Assets/scripts/Platform/PlatformSensor.cs
+++ Assets/scripts/Platform/PlatformSensor.cs
@@ -20,7 +20,7 @@ namespace Game{
 		}
 
 		void OnTriggerEnter2D(Collider2D ob) {
-			if (ob.CompareTag(Values.PLAYER_TAG)){
+			if (ob.CompareTag(Values.PLAYER_TAG) || ob.CompareTag(Values.SHELL_TAG)){
 //				Debug.Log("PlatformSensor: OnTriggerEnter");
 				Ray ray = new Ray(ob.transform.position, (transform.position - ob.transform.position));
 				bool isSide = CheckNormalIsSide(ray.direction);
@@ -33,7 +33,7 @@ namespace Game{
 		}
 
 		void OnTriggerExit2D(Collider2D ob) {
-			if (ob.CompareTag(Values.PLAYER_TAG)) {
+			if (ob.CompareTag(Values.PLAYER_TAG)  || ob.CompareTag(Values.SHELL_TAG)) {
 //				Debug.Log("PlatformSensor: OnTriggerExit");
 				Rigidbody2D rb = ob.GetComponent<Rigidbody2D>();
 				if (ob != null && rb != platform_view.body) {
--- Assets/scripts/Values.cs
+++ Assets/scripts/Values.cs
@@ -15,6 +15,7 @@ namespace Game{
 		public static string PLATFORM_BODY_TAG = "platform_body";
 		public static string PLAYER_TAG = "player";
 		public static string SHOT_TAG = "shot";
+		public static string SHELL_TAG = "shell";
 		public static string BOUNDRIES_TAG = "gameBoundry";
 		public static string WALL_TAG = "wall";
 
