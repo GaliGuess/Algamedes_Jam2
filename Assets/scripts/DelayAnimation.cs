658e99a8685a927085ec57909cabb3bf1d66cc42
GaliGuess <zgal10@gmail.com>
2018-02-07 16:19:32 +0200

    Added press PS to startScene

---
+++ Assets/scripts/DelayAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAnimation : MonoBehaviour {
+
+	public float secondsToDelay = 2;
+	private Animator animator;
+	
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+		animator.enabled = false;
+	}
+
+	void Start ()
+	{
+		StartCoroutine(showAnimation());
+	}
+
+
+	IEnumerator showAnimation()
+	{
+		yield return new WaitForSeconds(secondsToDelay);
+		animator.enabled = true;
+	}
+}
