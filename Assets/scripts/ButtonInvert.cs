021cb93ec146ee91ddafb08001552e5012da02cf
GaliGuess <zgal10@gmail.com>
2018-02-14 03:54:45 +0200

    Updated new level menu

---
+++ Assets/scripts/ButtonInvert.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInvert : MonoBehaviour
+{
+	private Image _image;
+	
+	
+	void Awake ()
+	{
+		_image = GetComponent<Image>();
+	}
+	
+	void Invert () {
+		
+		_image.color = new Color(1 - _image.color.r, 1 - _image.color.g, 1 - _image.color.b, _image.color.a);
+	}
+
+	private void OnMouseEnter()
+	{
+		Invert();
+	}
+
+	private void OnMouseExit()
+	{
+		Invert();
+	}
+}
---
+++ Assets/scripts/MenuButton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+ 
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+ 
+	private Image _image;
+	private Shader _shader;
+	
+	private Color currentColor;
+
+	private int textColor;
+
+	
+	private void Awake()
+	{
+		_image = GetComponent<Image>();
+//		Debug.Log(_image.name);
+//		_image.material.shader = Shader.Find("GUI/Text Shader");
+//		currentColor = _image.material.color;
+		currentColor = _image.color;
+
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Invert();
+	}
+ 
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Invert();
+	}
+	
+	
+	void Invert() {
+		
+		currentColor = new Color(1 - currentColor.r,
+							     1 - currentColor.g, 
+							     1 - currentColor.b, 
+								 1);
+		
+		_image.color = currentColor;
+//		_image.material.color = currentColor;
+	}
+}
