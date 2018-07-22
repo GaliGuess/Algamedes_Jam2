aa278093be0fc5bb5e458783f88123e918933676
mosem <mandelmoshe@gmail.com>
2018-07-22 13:00:49 +0300

    before activating toggle menu

--- Assets/scripts/Game/GameManager.cs
+++ Assets/scripts/Game/GameManager.cs
@@ -230,22 +230,34 @@ namespace Game {
 		public void TogglePauseMenu()
 		{
 			Debug.Log("GAMEMANAGER:: TogglePauseMenu");
-			if (_pause_menu == null) {
+//			if (_pause_menu == null) {
+//				return;
+//			}
+//			// not the optimal way but for the sake of readability
+//			if (_pause_menu.activeSelf)
+//			{
+//				_pause_menu.SetActive(false);
+//				Time.timeScale = 1.0f;
+//			}
+//			else
+//			{
+//				_pause_menu.SetActive(true);
+//				Time.timeScale = 0f;
+//			}
+
+			if (_endGameMenu == null) {
 				return;
 			}
-			// not the optimal way but for the sake of readability
-			if (_pause_menu.activeSelf)
-			{
-				_pause_menu.SetActive(false);
-				Time.timeScale = 1.0f;
+
+			if (_endGameMenu.activeSelf) {
+				_endGameMenu.SetActive(false);
+
 			}
-			else
-			{
-				_pause_menu.SetActive(true);
-				Time.timeScale = 0f;
+			else {
+				_endGameMenu.SetActive(true);
 			}
 
-			Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
+//			Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
 		}
 	}
 }
